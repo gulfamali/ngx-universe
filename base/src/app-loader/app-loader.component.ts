ed0400299006ac4533aec86162933e512cfe0627
Gulfam Ali <gulfam.work@gmail.com>
2024-07-19 22:18:54 +0530

    chore: few changes

---
+++ base/src/app-loader/app-loader.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'base-app-loader',
+  standalone: true,
+  imports: [],
+  styleUrl: './app-loader.component.css',
+  template: `@if (loading) {
+    <div class="loader-overlay">
+      <div class="linear"></div>
+    </div>
+  }`,
+})
+export class AppLoader {
+  @Input() loading: boolean = false;
+}
--- base/src/public-api.ts
+++ base/src/public-api.ts
@@ -4,3 +4,4 @@
 
 export * from './lib/base.service';
 export * from './lib/base.component';
+export * from './app-loader/app-loader.component';
